fbe44f451db805e6eab7c49af0119fd93e438c40
PerfectionistCoder <github.hug438@outlook.com>
2024-12-26 16:55:03 +0000

    return time left from light mod

--- src/light.rs
+++ src/light.rs
@@ -5,22 +5,20 @@ use anyhow::Error;
 
 #[derive(PartialEq, Eq)]
 pub enum LightMode {
-    Light,
-    Dark,
+    Light(Timestamp),
+    Dark(Timestamp),
 }
 
 impl LightMode {
-    fn decide_mode(
-        lat: Latitude,
-        lng: Longitude,
-        timestamp: Timestamp,
-    ) -> Result<Self, Error> {
-        let SunTime { sunrise, sunset } = SunTime::calc_time(lat, lng, timestamp)?;
-        Ok(if sunrise < timestamp && timestamp < sunset {
-            LightMode::Light
-        } else {
-            LightMode::Dark
-        })
+    fn decide_mode(lat: Latitude, lng: Longitude, timestamp: Timestamp) -> Result<Self, Error> {
+        let sun_time = SunTime::calc_time(lat, lng, timestamp)?;
+        Ok(
+            if sun_time.sunrise() < timestamp && timestamp < sun_time.sunset() {
+                LightMode::Light(sun_time.sunset() - timestamp)
+            } else {
+                LightMode::Dark(sun_time.sunrise() - timestamp)
+            },
+        )
     }
     pub fn get_mode(lat: Latitude, lng: Longitude) -> Result<Self, Error> {
         let now = get_current_timestamp()?;
@@ -31,29 +29,46 @@ impl LightMode {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::test_utils::{get_timestamp, LONDON};
+    use crate::test_utils::{get_timestamp, NAIROBI};
+    const HOUR: i64 = 3600;
 
-    #[test]
-    fn noon() {
-        let timestamp = get_timestamp(6, 12, LONDON.offset);
-        assert!(
-            LightMode::decide_mode(LONDON.lat, LONDON.lng, timestamp).unwrap() == LightMode::Light
-        );
-    }
+    mod test_light_mode_and_time_left {
+        use super::*;
 
-    #[test]
-    fn early_morning() {
-        let timestamp = get_timestamp(6, 3, LONDON.offset);
-        assert!(
-            LightMode::decide_mode(LONDON.lat, LONDON.lng, timestamp).unwrap() == LightMode::Dark
-        );
-    }
+        #[test]
+        fn noon() {
+            let timestamp = get_timestamp(6, 12, NAIROBI.offset);
+            if let LightMode::Light(time_left) =
+                LightMode::decide_mode(NAIROBI.lat, NAIROBI.lng, timestamp).unwrap()
+            {
+                assert!((time_left - 6 * HOUR).abs() < HOUR)
+            } else {
+                panic!()
+            }
+        }
 
-    #[test]
-    fn late_night() {
-        let timestamp = get_timestamp(6, 22, LONDON.offset);
-        assert!(
-            LightMode::decide_mode(LONDON.lat, LONDON.lng, timestamp).unwrap() == LightMode::Dark
-        );
+        #[test]
+        fn early_morning() {
+            let timestamp = get_timestamp(6, 3, NAIROBI.offset);
+            if let LightMode::Dark(time_left) =
+                LightMode::decide_mode(NAIROBI.lat, NAIROBI.lng, timestamp).unwrap()
+            {
+                assert!((time_left - 3 * HOUR).abs() < HOUR)
+            } else {
+                panic!()
+            }
+        }
+
+        #[test]
+        fn late_night() {
+            let timestamp = get_timestamp(6, 22, NAIROBI.offset);
+            if let LightMode::Dark(time_left) =
+                LightMode::decide_mode(NAIROBI.lat, NAIROBI.lng, timestamp).unwrap()
+            {
+                assert!((time_left - 8 * HOUR).abs() < HOUR)
+            } else {
+                panic!()
+            }
+        }
     }
 }
