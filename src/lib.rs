bdb99f121d486877d575f4d9e01a3ef8152dd2bf
PerfectionistCoder <github.hug438@outlook.com>
2024-12-26 17:24:33 +0000

    prototype run method

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,48 @@
-mod color;
-mod wayland;
-mod sunrise;
+use std::{thread::sleep, time::Duration};
 
-pub fn run() {}
+use config::Config;
+use light::LightMode::{self, Dark, Light};
+
+mod color;
+mod config;
+mod light;
+mod sun_time;
+mod wayland;
+
+pub fn run() {
+    let cfg = Config::new();
+    let mut time;
+    let (mut wayland, mut wayland_state) = wayland::Wayland::new().unwrap();
+
+    loop {
+        println!("{}", wayland_state.color_changed());
+
+        let mode = LightMode::get_mode(cfg.lat(), cfg.lng()).unwrap();
+        wayland_state = match mode {
+            Light(t) => {
+                let s = wayland_state.change_to_color(cfg.light_mode(), 0);
+                time = t;
+                s
+            }
+            Dark(t) => {
+                let s = wayland_state.change_to_color(cfg.dark_mode(), 0);
+                time = t;
+                s
+            }
+        };
+
+        loop {
+            if wayland.poll(&mut wayland_state).is_ok() {
+                println!("change");
+                break;
+            }
+            sleep(Duration::from_millis(50));
+        }
+
+        println!("wait {}", time);
+        sleep(Duration::from_secs(time as u64));
+    }
+}
+
+#[cfg(test)]
+mod test_utils;
