ca4ff5a1c66ce4182c2306baa27604ef893c1e0e
PerfectionistCoder <github.hug438@outlook.com>
2024-12-31 11:14:12 +0000

    main loop

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 use std::{
     sync::{Arc, Mutex},
-    thread::{self, sleep}, time::Duration,
+    thread::{self, sleep},
+    time::Duration,
 };
 
 use config::Config;
+use mode::{LightMode, ModeTimer};
 
 mod color;
 mod config;
@@ -11,20 +13,38 @@ mod mode;
 mod wayland;
 
 pub fn run() {
-    let cfg = Config::new(None).unwrap();
+    let cfg = Config::new(Some(String::from("example-config.ini"))).unwrap();
+
     let (mut wayland, wayland_state) = wayland::WaylandClient::new().unwrap();
     let state = Arc::new(Mutex::new(wayland_state));
 
-    let state1 = Arc::clone(&state);
-    thread::spawn(move || {
-        state1.lock().unwrap().change_to_color(cfg.dark_mode(), 3.0);
-    });
-    
+    {
+        let state = Arc::clone(&state);
+        thread::spawn(move || loop {
+            if state.lock().unwrap().color_changed() {
+                eprintln!("update color");
+                let mut state = state.lock().unwrap();
+                wayland.poll(&mut state).unwrap();
+            }
+        });
+    }
+
+    let mut first = true;
     loop {
-        if state.lock().unwrap().color_changed() {
-            let mut state = state.lock().unwrap();
-            let _ = wayland.poll(&mut state);
+        let timer = ModeTimer::new(cfg.location()).unwrap();
+        let mode = if timer.mode() == LightMode::Light {
+            cfg.light_mode()
+        } else {
+            cfg.dark_mode()
+        };
+
+        if first {
+            state.lock().unwrap().change_to_color(mode, 0.0);
+            first = false;
+        } else {
+            state.lock().unwrap().change_to_color(mode, cfg.animation().transition);
         }
-        sleep(Duration::from_millis(10));
+        eprintln!("wait: {}", timer.next());
+        sleep(Duration::from_secs(timer.next() as u64));
     }
 }
