d6d34625b0e0975fe0052009dd73f5657be9613b
PerfectionistCoder <github.hug438@outlook.com>
2025-04-10 23:30:43 +0100

    get gamma control & set output gamma

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::os::fd::{BorrowedFd, IntoRawFd};
+
 use wayland_client::{
     Connection, Dispatch, Proxy, QueueHandle,
     protocol::{
@@ -5,7 +7,14 @@ use wayland_client::{
         wl_registry,
     },
 };
-use wayland_protocols_wlr::gamma_control::v1::client::zwlr_gamma_control_manager_v1::ZwlrGammaControlManagerV1;
+use wayland_protocols_wlr::gamma_control::v1::client::{
+    zwlr_gamma_control_manager_v1::ZwlrGammaControlManagerV1,
+    zwlr_gamma_control_v1::{self, ZwlrGammaControlV1},
+};
+
+mod color;
+
+use color::{Color, colorramp_fill};
 
 #[derive(Debug)]
 struct WaylandState {
@@ -26,15 +35,35 @@ struct Output {
     global_name: u32,
     wl_output: WlOutput,
     event_name: Option<String>,
+    gamma_control: Option<ZwlrGammaControlV1>,
+    gamma_size: Option<usize>,
+    color: Color,
+    color_changed: bool,
 }
 
 impl Output {
-    pub fn new(global_name: u32, wl_output: WlOutput) -> Self {
+    fn new(global_name: u32, wl_output: WlOutput) -> Self {
         Self {
             global_name,
             wl_output,
             event_name: None,
+            gamma_control: None,
+            gamma_size: None,
+            color: Color::default(),
+            color_changed: false,
         }
+    }
+
+    fn set_gamma(&mut self) {
+        let file = shmemfdrs2::create_shmem(c"/ramp-buffer").unwrap();
+        file.set_len(self.gamma_size.unwrap() as u64 * 6).unwrap();
+        let mut mmap = unsafe { memmap2::MmapMut::map_mut(&file).unwrap() };
+        let buf = bytemuck::cast_slice_mut::<u8, u16>(&mut mmap);
+        let (r, rest) = buf.split_at_mut(self.gamma_size.unwrap());
+        let (g, b) = rest.split_at_mut(self.gamma_size.unwrap());
+        colorramp_fill(r, g, b, self.gamma_size.unwrap(), self.color);
+        let fd = unsafe { BorrowedFd::borrow_raw(file.into_raw_fd()) };
+        self.gamma_control.as_ref().unwrap().set_gamma(fd);
     }
 }
 
@@ -56,7 +85,6 @@ impl Dispatch<wl_registry::WlRegistry, ()> for WaylandState {
             if interface == WlOutput::interface().name {
                 let wl_output = registry.bind::<WlOutput, _, _>(name, version, qh, ());
                 state.outputs.push(Output::new(name, wl_output));
-
                 let mut event_queue = conn.new_event_queue();
                 event_queue.roundtrip(state).unwrap();
             } else if interface == ZwlrGammaControlManagerV1::interface().name {
@@ -74,7 +102,7 @@ impl Dispatch<WlOutput, ()> for WaylandState {
         event: <WlOutput as Proxy>::Event,
         _data: &(),
         _conn: &Connection,
-        _qhandle: &QueueHandle<Self>,
+        _qh: &QueueHandle<Self>,
     ) {
         if let wl_output::Event::Name { name } = event {
             let output = state
@@ -94,8 +122,31 @@ impl Dispatch<ZwlrGammaControlManagerV1, ()> for WaylandState {
         _event: <ZwlrGammaControlManagerV1 as Proxy>::Event,
         _data: &(),
         _conn: &Connection,
-        _qhandle: &QueueHandle<Self>,
+        _qh: &QueueHandle<Self>,
     ) {
+    }
+}
+
+impl Dispatch<ZwlrGammaControlV1, ()> for WaylandState {
+    fn event(
+        state: &mut Self,
+        proxy: &ZwlrGammaControlV1,
+        event: <ZwlrGammaControlV1 as Proxy>::Event,
+        _data: &(),
+        conn: &Connection,
+        _qh: &QueueHandle<Self>,
+    ) {
+        if let zwlr_gamma_control_v1::Event::GammaSize { size } = event {
+            let output = state
+                .outputs
+                .iter_mut()
+                .find(|o| o.gamma_control.as_ref().unwrap() == proxy)
+                .unwrap();
+            output.gamma_size = Some(size as usize);
+            output.set_gamma();
+            let mut event_queue = conn.new_event_queue();
+            event_queue.roundtrip(state).unwrap();
+        }
     }
 }
 
@@ -108,9 +159,16 @@ fn main() {
     let qh = event_queue.handle();
 
     let mut state = WaylandState::new();
-
     display.get_registry(&qh, ());
+    event_queue.roundtrip(&mut state).unwrap();
 
+    if let Some(gamma_manager) = &state.gamma_manager {
+        for output in &mut state.outputs {
+            let wl_output = &output.wl_output;
+            output.gamma_control = Some(gamma_manager.get_gamma_control(wl_output, &qh, ()));
+        }
+    }
     event_queue.roundtrip(&mut state).unwrap();
     println!("{:?}", state);
+    loop {}
 }
